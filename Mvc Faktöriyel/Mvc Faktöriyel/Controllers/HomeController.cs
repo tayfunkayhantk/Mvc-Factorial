59b02019a51a86265e8e4b7bbc2a805d193b3484
tayfunkayhantk <tayfunkayhantk@gmail.com>
2023-01-06 00:55:38 +0300

    MVC Factorial Calculation
    factorial calculation project

---
+++ Mvc Faktöriyel/Mvc Faktöriyel/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Faktöriyel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int number = 0)
+        {
+
+            int conclusion = 1;
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                conclusion = conclusion * i;
+            }
+
+
+            ViewBag.conc = conclusion;
+            return View();
+        }
+
+
+    }
+}
---
+++ Mvc Faktöriyel/Mvc Faktöriyel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc_Faktöriyel.MvcApplication" Language="C#" %>
